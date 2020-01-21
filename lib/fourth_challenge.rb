0cd15cafb111b7a6f86ac6e5665e57a7977d6eba
calm-fork-7535 <komald.4@gmail.com>
2020-01-21 17:11:21 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [{name: "Steven", age: "30", attitude: "confused"}]
+      :hero_friends => [{name: "Benvolio", "Mercutio" age: "17", "18" attitude: "worried", "hot-headed"}]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
@@ -14,6 +14,6 @@ def fourth_challenge
    }
 }
 
-
+#[{name: "Steven", age: "30", attitude: "confused"}]
 
 end
