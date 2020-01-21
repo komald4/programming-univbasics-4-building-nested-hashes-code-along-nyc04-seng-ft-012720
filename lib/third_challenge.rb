9e2f91c49e6be7151009079d555820d7ae3cf050
calm-fork-7535 <komald.4@gmail.com>
2020-01-21 16:59:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {name:"Lady Capulet", age: "50"}
-      :matriarch => {{name:"Lord Capulet", age: "51"}
+      :matriarch => {name:"Lord Capulet", age: "51"}
       :heroine => {name:"Juliet", age: "15", status: "alive"}
       :heroine_friends => []
    }
